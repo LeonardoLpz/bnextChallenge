f686397e80ca72f1ccb5f65148e79fcc711f6212
leonardo <leodanlors1993@gmail.com>
2020-02-20 16:03:42 -0600

    terminado estilo

--- functions.js
+++ functions.js
@@ -34,13 +34,23 @@ function requestData(){
 
     })
 
+
+
+   
+
     
+    
+
+            
 
 }
 //funcion que refresca la pagina
 function refresh(){
     location.reload();
 }
+
+
+
 
 
 // funcion que coloca la fecha actual
@@ -55,3 +65,87 @@ if (_mes < 10)
 else
 { var mes = _mes.toString;}
 document.getElementById("loan_date").min = anio+'-'+mes+'-'+dia; 
+
+
+
+// funcion para enviar datos qu se activa con el evento submit del formulario
+let myForm = document.querySelector('.form') //guardo mi formulario en una variable
+
+// en espera del evento submit
+myForm.addEventListener('submit',()=>{
+   
+    event.preventDefault()//evita la recarga de pagina al hacer submit
+        //se configura la dirección de la api para el POST 
+        let id = document.getElementById('number').value
+        let api_url = `https://api.bnext.io/partner_test/user/${id}`
+        
+            //se obtienen lo datos del formulario y se almacenan en variables para su uso posterior
+            let name = document.getElementById('name_received').value
+            let surname = document.getElementById('surname_received').value
+            let email = document.getElementById('email_received').value
+            let phone = document.getElementById('phone_received').value
+            let amount = document.getElementById('loan_amount').value
+            let date = document.getElementById('loan_date').value
+            let weeks = document.getElementById('loan_weeks').value
+            let age = document.getElementById('age_received').value
+    
+    
+        //uso de fetch para conectar al servidor 
+        fetch(api_url,{
+            method: 'POST', //metodo para envio de datos al server
+            headers: {
+                'X-WEB-KEY': 'Development'
+            },
+    
+            //se convierte el texto en formato JSON
+            body: JSON.stringify({
+                'id': id, 
+                'name':name, 
+                'surname': surname, 
+                'email':email,
+                'phone':phone,
+                'age':age,
+                'loan_amount':amount,
+                'loan_weeks':weeks,
+                'loan_date':date
+            }),
+            cache: 'no-cache'
+        })
+        //se espera la respuesta del servidor y se guarda en un JSON
+        .then(response => response.json())
+        //se confirma codigo del success para mostrar agradecimiento y resumen de los datos enviados
+        .then(data => {
+            console.log('data = ', data)
+            console.log(data['code'])
+            if(data['code']==201){
+                // funcion que muestra el mensaje de agradecimiento
+                document.querySelector('.userThanks').style.display ='grid'
+                document.querySelector('.form').style.display = 'none'
+                 
+              
+                
+            }
+    
+        })
+        //para el error al envio de los datos se presenta un mensaje
+
+        .catch(() => {
+        
+            document.querySelector('.userNo').style.display = 'grid' //muestra el mensaje de no encontrado
+            document.getElementById('data').style.display = 'none' //oculta el formulario
+    
+        })
+    
+        
+        
+    
+
+
+})
+
+
+
+
+
+
+
